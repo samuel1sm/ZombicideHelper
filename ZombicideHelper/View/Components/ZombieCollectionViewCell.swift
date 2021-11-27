47a4e922a0f69ce811a9e5dccd6fab7011fc6c21
Samuel Martins <francisco.martins@vaivoa.com>
2021-11-27 13:55:07 -0300

    Updated label to use constraints

--- ZombicideHelper/View/Components/ZombieCollectionViewCell.swift
+++ ZombicideHelper/View/Components/ZombieCollectionViewCell.swift
@@ -14,33 +14,39 @@ class ZombieCollectionViewCell: UICollectionViewCell {
         let zombieUIImage = UIImageView()
         zombieUIImage.contentMode = .scaleToFill
         zombieUIImage.clipsToBounds = true
-        zombieUIImage.layer.cornerRadius = 5
+        zombieUIImage.layer.cornerRadius = 10
         return zombieUIImage
     }()
     
     private let nameLabel : UILabel = {
         let nameLabel = UILabel()
         nameLabel.textAlignment = .center
-        nameLabel.backgroundColor = .green
-        nameLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        nameLabel.backgroundColor = .systemGreen
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 25)
         nameLabel.adjustsFontSizeToFitWidth = true
-            
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
         return nameLabel
     }()
     
     override init(frame: CGRect){
         super.init(frame: frame)
-        self.layer.cornerRadius = 10
-//        contentView.backgroundColor = .systemRed
         self.backgroundView = zombieImageView
-//        contentView.addSubview(zombieImageView)
         contentView.addSubview(nameLabel)
         contentView.clipsToBounds = true
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        nameLabel.frame = CGRect(x: 5, y: contentView.frame.size.height-50, width: contentView.frame.size.width-10, height: 50)
+        nameLabel.layer.cornerRadius = 10
+        
+        NSLayoutConstraint.activate([
+            nameLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -6),
+            nameLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant:5),
+            nameLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -5),
+            nameLabel.topAnchor.constraint(greaterThanOrEqualTo: self.safeAreaLayoutGuide.topAnchor, constant: 0),
+            nameLabel.centerXAnchor.constraint(equalTo:self.centerXAnchor , constant: 0)
+        ])
+
 //        zombieImageView.frame = CGRect(x: 5, y: 0, width: contentView.frame.size.width-10, height: contentView.frame.size.height-50)
         
 //        zombieImageView.frame = CGRect(x: 5, y: 0, width: contentView.frame.size.width, height: contentView.frame.size.height)
