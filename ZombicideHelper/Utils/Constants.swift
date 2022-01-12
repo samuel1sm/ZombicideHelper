863527c40473aa1b52a90c440f9fb96da5c17659
Samuel Martins <francisco.martins@vaivoa.com>
2022-01-12 10:12:00 -0300

    Creating constants

---
+++ ZombicideHelper/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ZombicideHelper
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 12/01/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let searchIcon = "magnifyingglass"
+}
